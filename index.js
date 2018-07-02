fa4a30c901596cf9dbeff060889c7f4d9e581400
ali bagho <ali.bagho@gmail.com>
2018-07-02 19:26:20 +0100

    hashage

--- index.js
+++ index.js
@@ -7,47 +7,53 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const sequelize = require('sequelize');
+const bc = require('bcrypt');
 const op = sequelize.Op;
 
+// use JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-  app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: 200 // legacy browser (IE11) chokes on 204 
-  }))
+// CORS
+app.use(cors({
+origin: '*',
+optionsSuccessStatus: 200 // legacy browser (IE11) chokes on 204 
+}))
 
 app.post('/login', (req, res) => {
     if(req.body.login && req.body.password)
     {
-    models.user.findOne({
-        where:{
-            [op.and]: {
-                login: req.body.login,
-                password: req.body.password
+        models.user.findOne({
+            where:{
+                [op.and]: {
+                    login: req.body.login,
+                    //password: req.body.password
+                }
             }
-        }
-    })
-    .then((user) => {
-        console.log("wtf");
-        if(user) {
-            jwt.sign({user: user}, 'secretkey', (err, token) => {
-                res.json({"token": token,"error":""});
-            })
-        }
-        else {
-            res.json({"error":"Wrong combination"});
-        }
-    })
-    .catch((err) => {
-        res.status(500).json({"error":"Internal server error"});
-    })
-}
+        })
+        .then((user) => {
+            if(user) {
+                // check password 
+                bc.compare(req.body.password, user.password, function(err, response) {
+                    console.log(user.password + " " + user.password);
+                    if (response) {
+                        jwt.sign({user: user}, 'secretkey', (err, token) => {
+                            res.json({"token": token,"error":""});
+                        })
+                    } else {
+                        res.json({"error":"Wrong combination"});
+                    }
+                });
+            } else {
+                res.json({"error":"Wrong combination"});
+            }
+
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({"error":"Internal server error"});
+        })
+    }
 else {
     res.json({"error":"Fill in the fields"});
 }
@@ -73,33 +79,36 @@ app.post('/register', function(req, res) {
             })
             .then((FoundUser)=> { 
                 if(FoundUser && FoundUser.dataValues.login == req.body.login){
-                    console.log("im 1 ");
                     res.status(200).json({error: "Login already in use",
                         message: 0
                     })
                 }
                 else if (FoundUser && FoundUser.dataValues.email == req.body.email) {
-                    console.log("im 2");
                     res.status(200).json({error: "Email already in use",
                         message: 0
                     })
                 }
                 else {
-                    console.log("and 3");
-                    models.user.create({
-                        login: req.body.login,
-                        email: req.body.email,
-                        password: req.body.password
-                    }).then(() => res.status(201).json({error: "",
-                                                        message: 1
-                                                        }))
-                    .catch((err) => res.status(500).json({error: "An error has occured",
-                                                            message: 0
-                                                        }))
+                    // TODO encrypt 
+                    const saltRounds = 10;
+
+                    bc.hash(req.body.password, saltRounds, function(err, hash) {
+                        // Store hash in your password DB.
+                        models.user.create({
+                            login: req.body.login,
+                            email: req.body.email,
+                            password: hash
+                        }).then(() => res.status(201).json({error: "",
+                                                            message: 1
+                                                            }))
+                        .catch((err) => res.status(500).json({error: "An error has occured",
+                                                                message: 0
+                                                            }))
+                      });
+
                 }
             })
             .catch((err) => {
-                console.log("4");
                 res.status(500).json({error: "An internal error has occured",
                                                     message: 0
                                                 })
